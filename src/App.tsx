67aa6d154f0c4785086674cca5cb23220e2c61dd
Ryan Kim <ryank9462@gmail.com>
2022-07-01 12:17:03 -0500

    remove @import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { MainContainer } from './components/MainContainer';
 
 export const StyledApp = styled.div`
-  @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300;500;600;800&display=swap');
   background-color: hsl(217, 19%, 24%);
   height: 100vh;
   padding: 0;
